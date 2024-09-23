0b7f19d17633d4449b295b01725ef56fcedf505c
keyfarel <carinsmddd@gmail.com>
2024-09-23 18:54:30 +0700

    push js php-2 soal 7

---
+++ P5/Soal 007/rekursif.php
@@ -0,0 +1,10 @@
+<?php
+function tampilkanAngka($jumlah, $idx = 1){
+    echo "Perulangan ke-: {$idx}<br>}";
+
+    if ($idx < $jumlah) {
+        tampilkanAngka($jumlah, $idx + 1);
+    }
+}
+
+tampilkanAngka(20);
