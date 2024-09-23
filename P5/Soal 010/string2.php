957ba717ad00cc0f39673ad7e5ed3908aa82acaa
keyfarel <carinsmddd@gmail.com>
2024-09-23 19:06:52 +0700

    push js php-2 soal 10

---
+++ P5/Soal 010/string2.php
@@ -0,0 +1,12 @@
+<?php
+echo "Baris\nbaru <br>";
+echo 'Baris\nbaru <br>';
+echo "Halo\nDunia <br>";
+echo 'Halo\nDunia <br>';
+
+echo "<pre>Halo\nDunia </pre>";
+echo '<pre>Halo\nDunia </pre>';
+
+echo "Katakanlah\nTidak ada narkoba";
+echo "<br/>";
+echo 'Katakanlah\nTidak ada narkoba';
