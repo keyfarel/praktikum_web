abcad4fec1a05879d28466875f4a5d1c5bf6dd3b
keyfarel <carinsmddd@gmail.com>
2024-09-24 15:47:37 +0700

    push js-2 php-2 part-2 soal-2

---
+++ P_007/Soal 002/array_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<?php
+$Dosen = [
+    'nama' => 'Elok Nur Hamdana',
+    'domisili' => 'Malang',
+    'Jenis Kelamin' => 'Perempuan'];
+echo "nama : {$Dosen ['nama']} <br>";
+echo "Domisili : {$Dosen ['domisili']} <br>";
+echo "Jenis Kelamin : {$Dosen ['Jenis Kelamin']} <br>";
+
+
+?>
+
+</body>
+</html>
