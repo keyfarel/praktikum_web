0e7617ba1a2ef61dfc5d0a95b5441f013aaae7fe
keyfarel <carinsmddd@gmail.com>
2024-09-24 15:46:18 +0700

    push js-2 php-2 part-2

---
+++ P_007/Soal 001/array_1.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+<?php
+$Listdosen = ["Elok Nur Hamdana", "Unggul Pemenang", "Bagas Nugraha"];
+
+echo $Listdosen[2] . "<br>";
+echo $Listdosen[0] . "<br>";
+echo $Listdosen[1] . "<br>";
+?>
+</body>
+</html>
